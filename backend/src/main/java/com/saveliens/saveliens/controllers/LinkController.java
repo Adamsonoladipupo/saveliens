ccc287e0bb55e1b358ce151e2c14214a35e62b8b
Adamsonoladipupo <adamsonoladipupo@gmail.com>
2026-04-29 14:28:09 +0100

    added link controller

---
+++ backend/src/main/java/com/saveliens/saveliens/controllers/LinkController.java
@@ -0,0 +1,44 @@
+package com.saveliens.saveliens.controllers;
+
+import com.saveliens.saveliens.dtos.requests.AddLinkRequestDto;
+import com.saveliens.saveliens.dtos.responses.AddLinkResponseDto;
+import com.saveliens.saveliens.services.LinkService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/topic/{topicId}/links")
+public class LinkController {
+    @Autowired
+    private LinkService linkService;
+
+    @GetMapping("/greet")
+    public String greet(){
+        return "Hey Link!, lets get the party started";
+    }
+
+    @PostMapping
+    public ResponseEntity<AddLinkResponseDto> addLink(@RequestBody AddLinkRequestDto requestDto, @PathVariable String topicId) {
+        AddLinkResponseDto response = linkService.addLink(requestDto, topicId);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<AddLinkResponseDto>> getLinks(@PathVariable String topicId) {
+        return ResponseEntity.ok(linkService.getAllUserLinks(topicId));
+    }
+
+    @DeleteMapping("/{linkId}")
+    public ResponseEntity<Void> deleteLink(@PathVariable String topicId, @PathVariable String linkId) {
+        linkService.deleteLink(linkId);
+        return ResponseEntity.noContent().build();
+    }
+
+
+
+}
