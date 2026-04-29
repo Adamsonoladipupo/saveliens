37ad4ceabf195caf910d4b59e9dc825466fb7b47
Adamsonoladipupo <adamsonoladipupo@gmail.com>
2026-04-29 14:27:41 +0100

    added dashboard controller

---
+++ backend/src/main/java/com/saveliens/saveliens/controllers/DashboardController.java
@@ -0,0 +1,23 @@
+package com.saveliens.saveliens.controllers;
+
+import com.saveliens.saveliens.dtos.responses.DashboardResponseDto;
+import com.saveliens.saveliens.services.DashboardService;
+import com.saveliens.saveliens.services.TopicService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/dashboard")
+public class DashboardController {
+    @Autowired
+    private DashboardService dashboardService;
+
+    @GetMapping("/me")
+    public ResponseEntity<DashboardResponseDto> getDashboard() {
+        return ResponseEntity.ok(dashboardService.getDashboard());
+    }
+
+}
