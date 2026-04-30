5137f9e22a518acf120122d3023e6d2ddb158a0e
Adamsonoladipupo <adamsonoladipupo@gmail.com>
2026-04-30 06:33:55 +0100

    updated source

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+import Router from './routes/router'
+import { RouterProvider } from 'react-router'
+
+function App() {
+  return (
+    <>
+      <RouterProvider router={Router}/>
+    </>
+  )
+}
+export default App
---
+++ frontend/src/routes/router.jsx
@@ -0,0 +1,25 @@
+import React from "react"
+import { createBrowserRouter } from "react-router"
+import Header from "../components/Header"
+import LandingPage from "../pages/landingPage"
+import SignUp from "../pages/SignUp"
+import SignIn from "../pages/SignIn"
+const Router = createBrowserRouter ([
+    {
+        path : "header",
+        element:<Header/>
+    },
+    {
+        path: "/",
+        element:<LandingPage/>
+    },
+    {
+        path:"signup",
+        element:<SignUp/>
+    },
+    {
+        path:"signin",
+        element:<SignIn/>
+    }
+])
+export default Router 
